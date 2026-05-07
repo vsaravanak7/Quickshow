657eb7ef81c1efd1e7d670262133bc821ce029fd
vsaravanak7 <saravannitt7@gmail.com>
2026-05-07 20:48:30 +0530

    webhook changes

--- server/routes/webhooks.js
+++ server/routes/webhooks.js
@@ -4,14 +4,29 @@ import { inngest } from "../inngest/index.js";
 const webhookRouter = express.Router();
 
 webhookRouter.post("/clerk", async (req, res) => {
-    const event = req.body;
 
-    await inngest.send({
-        name: event.type,
-        data: event.data,
-    });
+    try {
 
-    res.status(200).send("Webhook received");
+        const event = req.body;
+
+        console.log("Webhook received:", event.type);
+
+        await inngest.send({
+            name: event.type,
+            data: event.data,
+        });
+
+        console.log("Event sent to Inngest");
+
+        res.status(200).send("Webhook received");
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(500).send("Error");
+
+    }
 });
 
 export default webhookRouter;
