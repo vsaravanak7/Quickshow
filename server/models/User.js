1d829e79da05236191e265ab4a0263bc745cf28e
vsaravanak7 <saravannitt7@gmail.com>
2026-05-07 19:34:28 +0530

    model import fix

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
     image: {type: String, required: true},
-})
+});
 
 const User = mongoose.model('User', userSchema)
 
